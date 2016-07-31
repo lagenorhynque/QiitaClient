b6db94350adf3300fa1fd4d27d7e634d5e3cbd71
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-07-31 12:17:13 +0900

    add ListView

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -2,6 +2,7 @@ package sample.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
 import sample.qiitaclient.view.ArticleView
@@ -9,13 +10,20 @@ import sample.qiitaclient.view.ArticleView
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-    val articleView = ArticleView(applicationContext)
-    articleView.setArticle(Article(
-        id = "123",
-        title = "Kotlin入門",
-        url = "http://www.example.com/articles/123",
-        user = User(id = "456", name = "たろう", profileImageUrl = "")))
-    setContentView(articleView)
+    val listAdapter = ArticleListAdapter(applicationContext)
+    listAdapter.articles = listOf(
+        dummyArticle("Kotlin入門", "たろう"),
+        dummyArticle("Java入門", "じろう")
+    )
+    val listView: ListView = findViewById(R.id.list_view) as ListView
+    listView.adapter = listAdapter
   }
+
+  private fun dummyArticle(title: String, userName: String): Article =
+      Article(id = "",
+              title = title,
+              url = "https://kotlinlang.org/",
+              user = User(id = "", name = userName, profileImageUrl = ""))
 }
